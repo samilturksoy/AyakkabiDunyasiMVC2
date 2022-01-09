3e5bb2f2154e26793cd53bb33d18ba26e57144bf
Samil Turksoy <Samil.Turksoy@unilever.com>
2022-01-09 15:22:32 +0300

    Add project files.

---
+++ AyakkabiDunyasi/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AyakkabiDunyasi
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Content/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/custom.css"));
+        }
+    }
+}
---
+++ AyakkabiDunyasi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AyakkabiDunyasi.MvcApplication" Language="C#" %>
